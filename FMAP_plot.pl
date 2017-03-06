199a091a98ab0d5ff500882df9e2e712801edce7
jiwoongbio <jiwoongbio@gmail.com>
2017-03-06 17:06:40 -0600

    Edit FMAP_plot.pl and README.md

--- FMAP_plot.pl
+++ FMAP_plot.pl
@@ -4,37 +4,35 @@ use warnings;
 use Getopt::Long;
 use Statistics::R;
 
-my %parameterHash = ('width' => 1200, 'height' => 800, 'pointsize' => 20, 'leftmargin' => 20);
+my %parameterHash = ('width' => 12, 'height' => 8, 'leftmargin' => 20);
 GetOptions('h' => \(my $help = ''),
 	'w|width=i' => \$parameterHash{'width'},
 	'h|height=i' => \$parameterHash{'height'},
-	'p|pointsize=i' => \$parameterHash{'pointsize'},
 	'l|leftmargin=f' => \$parameterHash{'leftmargin'},
-	'v|pvalue=f' => \(my $pvalueCutoff = 0.05),
+	'p|pvalue=f' => \(my $pvalueCutoff = 0.05),
 	'c|coverage=f' => \(my $coverageCutoff = -1),
 	'd' => \(my $doNotPrintDefinition = ''),
 );
 if($help || scalar(@ARGV) == 0) {
 	die <<EOF;
 
-Usage:   perl FMAP_plot.pl [options] pathway.txt|module.txt|operon.txt plot.png
+Usage:   perl FMAP_plot.pl [options] pathway.txt|module.txt|operon.txt plot.pdf
 
 Options: -h       display this help message
          -w INT   plot width [$parameterHash{'width'}]
          -h INT   plot height [$parameterHash{'height'}]
-         -p INT   plot point size [$parameterHash{'pointsize'}]
          -l FLOAT plot left margin [$parameterHash{'leftmargin'}]
-         -v FLOAT p-value cutoff [$pvalueCutoff]
+         -p FLOAT p-value cutoff [$pvalueCutoff]
          -c FLOAT coverage cutoff [0 for pathway, 1 for module and operons]
          -d       do not print definition
 
 EOF
 }
-my ($inputFile, $pngFile) = @ARGV;
+my ($inputFile, $pdfFile) = @ARGV;
 foreach($inputFile) {
 	die "ERROR: '$_' is not readable.\n" unless(-r $_);
 }
-foreach($pngFile) {
+foreach($pdfFile) {
 	my $directory = /^(.*\/)/ ? $1 : '.';
 	die "ERROR: '$directory' is not a writable directory.\n" unless(-d $directory && -w $directory);
 }
@@ -74,7 +72,7 @@ my %pvalueHash = ();
 		my ($name, $pvalue) = @$_;
 		$R->run(sprintf('counts["%s"] <- %f', $name, -(log($pvalue) / log(10))));
 	}
-	$R->run(sprintf('png(filename = "%s", width = %d, height = %d, pointsize = %d)', $pngFile, @parameterHash{'width', 'height', 'pointsize'}));
+	$R->run(sprintf('pdf(file = "%s", width = %d, height = %d)', $pdfFile, @parameterHash{'width', 'height'}));
 	$R->run(sprintf('par(mar = c(par()$mar[1], par()$mar[2] + %f, par()$mar[3], par()$mar[4]))', $parameterHash{'leftmargin'}));
 	$R->run('barplot(counts, horiz = TRUE, border = NA, xlab = "-log10(p-value)", las = 1)');
 	$R->run('dev.off()');
