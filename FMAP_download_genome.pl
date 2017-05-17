9a481cf91436c278ef227afa4da511a5a900a552
jiwoongbio <jiwoongbio@gmail.com>
2017-05-17 17:42:31 -0500

    Input scientific name

--- FMAP_download_genome.pl
+++ FMAP_download_genome.pl
@@ -11,7 +11,7 @@ GetOptions('h' => \(my $help = ''),
 if($help || scalar(@ARGV) == 0) {
 	die <<EOF;
 
-Usage:   perl FMAP_download_genome.pl [options] NCBI_TaxID [...] > genome.fasta
+Usage:   perl FMAP_download_genome.pl [options] NCBI_TaxID|scientific_name [...] > genome.fasta
 
 Options: -h       display this help message
          -a       assembly instead of genome
@@ -19,62 +19,75 @@ Options: -h       display this help message
 EOF
 }
 my (@taxonIdList) = @ARGV;
-die "ERROR in $0: Taxon ID must be integer.\n" if(grep {$_ !~ /^[0-9]+$/} @taxonIdList);
+my $baseURL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils';
 if(@taxonIdList) {
-	@taxonIdList = sort {$a <=> $b} @taxonIdList;
+	@taxonIdList = sort {$a <=> $b} map {$_ =~ /^[0-9]*$/ ? $_ : esearch('taxonomy', "\"$_\"[Scientific Name]")} @taxonIdList;
 	@taxonIdList = @taxonIdList[0, grep {$taxonIdList[$_ - 1] != $taxonIdList[$_]} 1 .. $#taxonIdList];
 }
-
-my $baseURL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils';
-foreach my $taxonId (@taxonIdList){
+foreach my $taxonId (@taxonIdList) {
 	my %paramHash = ('db' => 'nuccore', 'usehistory' => 'y', 'retmax' => 500, 'rettype' => 'fasta', 'retmode' => 'text');
-	if($assemblyInsteadOfGenome) {
-		$paramHash{'term'} = sprintf('txid%d[Organism:exp] AND nucleotide_assembly[Filter] AND RefSeq[Filter]', $taxonId);
-	} else {
-		$paramHash{'term'} = sprintf('txid%d[Organism:exp] AND nucleotide_genome[Filter] AND RefSeq[Filter]', $taxonId);
-	}
-	{
-		my $queryString = join('&', map {"$_=$paramHash{$_}"} 'db', 'term', 'usehistory');
-		my $xmlString = `wget --no-verbose -O - '$baseURL/esearch.fcgi?$queryString'`;
-		if($xmlString ne '') {
-			my $dom = XML::LibXML->load_xml(string => $xmlString);
-			my $root = $dom->documentElement();
-			unless(getChildNodeList($root, 'ERROR')) {
-				($paramHash{'count'}) = map {$_->textContent} getChildNodeList($root, 'Count');
-				($paramHash{'WebEnv'}) = map {$_->textContent} getChildNodeList($root, 'WebEnv');
-				($paramHash{'query_key'}) = map {$_->textContent} getChildNodeList($root, 'QueryKey');
-			}
-		}
-		sleep(1);
-	}
-	my $count = 0;
-	if(defined($paramHash{'count'}) && defined($paramHash{'WebEnv'}) && defined($paramHash{'query_key'})) {
-		for($paramHash{'retstart'} = 0; $paramHash{'retstart'} < $paramHash{'count'}; $paramHash{'retstart'} += $paramHash{'retmax'}) {
-			my $queryString = join('&', map {"$_=$paramHash{$_}"} 'db', 'WebEnv', 'query_key', 'retstart', 'retmax', 'rettype', 'retmode');
-			open(my $reader, "wget --no-verbose -O - '$baseURL/efetch.fcgi?$queryString' |");
-			my @lineList = ();
-			while(my $line = <$reader>) {
-				next if($line eq "\n");
-				if($line =~ s/^>/>$taxonId:/) {
-					if(scalar(@lineList) > 1) {
-						print @lineList;
-						$count += 1;
-					}
-					@lineList = ();
+	foreach my $type ('genome', 'assembly') {
+		next if($type eq 'genome' && $assemblyInsteadOfGenome);
+		$paramHash{'term'} = sprintf('txid%d[Organism:exp] AND nucleotide_%s[Filter] AND RefSeq[Filter]', $taxonId, $type);
+		{
+			my $queryString = join('&', map {"$_=$paramHash{$_}"} 'db', 'term', 'usehistory');
+			my $xmlString = `wget --no-verbose -O - '$baseURL/esearch.fcgi?$queryString'`;
+			if($xmlString ne '') {
+				my $dom = XML::LibXML->load_xml(string => $xmlString);
+				my $root = $dom->documentElement();
+				unless(getChildNodeList($root, 'ERROR')) {
+					($paramHash{'count'}) = map {$_->textContent} getChildNodeList($root, 'Count');
+					($paramHash{'WebEnv'}) = map {$_->textContent} getChildNodeList($root, 'WebEnv');
+					($paramHash{'query_key'}) = map {$_->textContent} getChildNodeList($root, 'QueryKey');
 				}
-				push(@lineList, $line);
 			}
-			if(scalar(@lineList) > 1) {
-				print @lineList;
-				$count += 1;
-			}
-			close($reader);
 			sleep(1);
 		}
-	} else {
-		die "ERROR in $0: Download Failed\n";
+		my $count = 0;
+		if(defined($paramHash{'count'}) && defined($paramHash{'WebEnv'}) && defined($paramHash{'query_key'})) {
+			for($paramHash{'retstart'} = 0; $paramHash{'retstart'} < $paramHash{'count'}; $paramHash{'retstart'} += $paramHash{'retmax'}) {
+				my $queryString = join('&', map {"$_=$paramHash{$_}"} 'db', 'WebEnv', 'query_key', 'retstart', 'retmax', 'rettype', 'retmode');
+				open(my $reader, "wget --no-verbose -O - '$baseURL/efetch.fcgi?$queryString' |");
+				my @lineList = ();
+				while(my $line = <$reader>) {
+					next if($line eq "\n");
+					if($line =~ s/^>/>$taxonId:/) {
+						if(scalar(@lineList) > 1) {
+							print @lineList;
+							$count += 1;
+						}
+						@lineList = ();
+					}
+					push(@lineList, $line);
+				}
+				if(scalar(@lineList) > 1) {
+					print @lineList;
+					$count += 1;
+				}
+				close($reader);
+				sleep(1);
+			}
+		} else {
+			die "ERROR in $0: Download Failed\n";
+		}
+		print STDERR "$count $type sequences of taxon $taxonId are downloaded.\n";
+		last if($count > 0);
 	}
-	print STDERR "$count genomes of taxon $taxonId are downloaded.\n";
+}
+
+sub esearch {
+	my ($db, $term) = @_;
+	my @idList = ();
+	my $xmlString = `wget --no-verbose -O - '$baseURL/esearch.fcgi?db=$db&term=$term'`;
+	if($xmlString ne '') {
+		my $dom = XML::LibXML->load_xml(string => $xmlString);
+		my $root = $dom->documentElement();
+		@idList = map {$_->textContent} getChildNodeList($root, 'IdList', 'Id');
+	} else {
+		die "ERROR in $0: Search Failed\n";
+	}
+	sleep(1);
+	return @idList;
 }
 
 sub getChildNodeList {
