1bc5ba1877af6fde88b5ef298a7f64a342d2377a
jiwoongbio <jiwoongbio@gmail.com>
2017-05-17 17:51:08 -0500

    Input FMAP_operon.pl output

--- FMAP_assembly_operon.pl
+++ FMAP_assembly_operon.pl
@@ -12,7 +12,7 @@ GetOptions('h' => \(my $help = ''), 'a' => \(my $all = ''));
 if($help || scalar(@ARGV) == 0) {
 	die <<EOF;
 
-Usage:   perl FMAP_assembly_operon.pl [options] FMAP_assembly.region.txt > FMAP_assembly_operon.txt
+Usage:   perl FMAP_assembly_operon.pl [options] FMAP_assembly.region.txt [FMAP_operon.txt] > FMAP_assembly_operon.txt
 
 Options: -h       display this help message
          -a       print single-gene operons as well
@@ -20,41 +20,57 @@ Options: -h       display this help message
 EOF
 }
 
-my ($regionFile) = @ARGV;
+my ($regionFile, $operonFile) = @ARGV;
 foreach($regionFile, "$fmapPath/FMAP_data/known_operon.KEGG_orthology.txt") {
 	die "ERROR in $0: '$_' is not readable.\n" unless(-r $_);
 }
 my %orthologyContigStrandIndexStartEndListHash = ();
 {
-	open(my $reader, "sort --field-separator='\t' -k1,1 -k4,4 -k2,2n -k3,3n $regionFile |");
-	my ($contigStrand, @orthologyStartEndList) = ('');
+	my %contigStrandOrthologyStartEndListHash = ();
+	open(my $reader, $regionFile);
 	chomp(my $line = <$reader>);
 	my @columnList = split(/\t/, $line);
 	while(my $line = <$reader>) {
 		chomp($line);
 		my %tokenHash = ();
 		@tokenHash{@columnList} = split(/\t/, $line);
-		if(join("\t", @tokenHash{'contig', 'strand'}) ne $contigStrand) {
-			addOrthology($contigStrand, @orthologyStartEndList) if($contigStrand ne '');
-			($contigStrand, @orthologyStartEndList) = (join("\t", @tokenHash{'contig', 'strand'}));
-		}
-		push(@orthologyStartEndList, [@tokenHash{'orthology', 'start', 'end'}]);
+		my $contigStrand = join("\t", @tokenHash{'contig', 'strand'});
+		push(@{$contigStrandOrthologyStartEndListHash{$contigStrand}}, [@tokenHash{'orthology', 'start', 'end'}]);
 	}
-	addOrthology($contigStrand, @orthologyStartEndList) if($contigStrand ne '');
 	close($reader);
-
-	sub addOrthology {
-		my ($contigStrand, @orthologyStartEndList) = @_;
+	foreach my $contigStrand (sort keys %contigStrandOrthologyStartEndListHash) {
+		my @orthologyStartEndList = @{$contigStrandOrthologyStartEndListHash{$contigStrand}};
+		my ($contig, $strand) = split(/\t/, $contigStrand);
+		@orthologyStartEndList = sort {$a->[1] <=> $b->[1]} @orthologyStartEndList if($strand eq '+');
+		@orthologyStartEndList = sort {$a->[2] <=> $b->[2]} @orthologyStartEndList if($strand eq '-');
 		foreach my $index (0 .. $#orthologyStartEndList) {
 			my ($orthology, $start, $end) = @{$orthologyStartEndList[$index]};
 			push(@{$orthologyContigStrandIndexStartEndListHash{$orthology}}, [$contigStrand, $index, $start, $end]);
 		}
 	}
 }
+my %operonsOrthologiesTokenListHash = ();
+if(defined($operonFile)) {
+	open(my $reader, $operonFile);
+	chomp(my $line = <$reader>);
+	my @columnList = split(/\t/, $line);
+	while(my $line = <$reader>) {
+		chomp($line);
+		my %tokenHash = ();
+		@tokenHash{@columnList} = split(/\t/, $line);
+		my ($operons, $orthologies) = @tokenHash{'operons', 'orthologies'};
+		$operonsOrthologiesTokenListHash{$operons}->{$orthologies} = [@tokenHash{'definition', 'log2foldchange', 'coverage', 'pvalue', 'pathways'}];
+	}
+	close($reader);
+}
 {
 	open(my $reader, "$fmapPath/FMAP_data/known_operon.KEGG_orthology.txt");
 	open(my $writer, "| sort --field-separator='\t' -k1,1 -k2,2n -k3,3n -k4");
-	print $writer join("\t", 'contig', 'start', 'end', 'strand', 'operons', 'orthologies', 'regions'), "\n";
+	if(defined($operonFile)) {
+		print $writer join("\t", 'contig', 'start', 'end', 'strand', 'operons', 'orthologies', 'regions', 'definition', 'log2foldchange', 'coverage', 'pvalue', 'pathways'), "\n";
+	} else {
+		print $writer join("\t", 'contig', 'start', 'end', 'strand', 'operons', 'orthologies', 'regions'), "\n";
+	}
 	while(my $line = <$reader>) {
 		chomp($line);
 		my ($operons, $orthologies) = split(/\t/, $line);
@@ -86,7 +102,11 @@ my %orthologyContigStrandIndexStartEndListHash = ();
 						@orthologyStartEndList = reverse(@orthologyStartEndList) if($strand eq '-');
 						my $orthologies = join(' ', map {$_->[0]} @orthologyStartEndList);
 						my $regions = join(' ', map {"$_->[1]-$_->[2]"} @orthologyStartEndList);
-						print $writer join("\t", $contig, $start, $end, $strand, $operons, $orthologies, $regions), "\n";
+						if(defined($operonFile)) {
+							print $writer join("\t", $contig, $start, $end, $strand, $operons, $orthologies, $regions, @{$operonsOrthologiesTokenListHash{$operons}->{$orthologies}}), "\n";
+						} else {
+							print $writer join("\t", $contig, $start, $end, $strand, $operons, $orthologies, $regions), "\n";
+						}
 					}
 				}
 			}
